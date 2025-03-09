57836212a2fea522edf77695b02ed9fc83d16b03
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-03-09 20:22:16 +0630

    Extending Styles

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,6 +6,13 @@ import Icon from './Icon';
 const Title = styled.h1`
   color: #aac9f0;
 `;
+const SubTitle = styled(Title)`
+  font-size: 12px;
+  align-self: center;
+`
+const ParagraphTitle = styled(SubTitle)`
+  color: gray;
+`
 const Section = styled.div`
   background-color: #f0d1aa;
   display: flex;
@@ -18,10 +25,12 @@ const Content = () => {
   return (
     <Section>
       <Title>💅 Section</Title>
+      <SubTitle>I am a subtitle</SubTitle>
       <Button primary text="me first" />
       <Button text="me second" />
       <Icon primary/>
       <Icon />
+      <ParagraphTitle>I am a paragraph</ParagraphTitle>
     </Section>
   );
 };
